5439fa46bd2436b944f5a31197750d575743b787
OlegSofonov <olesof@mail.ru>
2024-01-14 12:33:29 +0500

    update Cart.php for Githab Actions

--- src/Cart.php
+++ src/Cart.php
@@ -6,22 +6,22 @@ class Cart
 {
     private $items = [];
 
-    public function addItem($good, $count)
+    public function addItem(array $good, int $count): void
     {
         $this->items[] = ['good' => $good, 'count' => $count];
     }
 
-    public function getItems()
+    public function getItems(): array
     {
         return $this->items;
     }
 
-    public function getCount()
+    public function getCount(): int
     {
         return array_reduce($this->items, fn ($acc, $item) => $acc + $item['count'], 0);
     }
 
-    public function getCost()
+    public function getCost(): int
     {
         return array_reduce($this->items, fn ($acc, $item) => $acc + $item['good']['price'] * $item['count']);
     }
